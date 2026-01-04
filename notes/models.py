1c6ae7cb2252b406bb50f4b5463eb2e14b4ac9a3
ahmedmohamedezz <ahmedmohamedezz111@gmail.com>
2026-01-04 17:57:44 +0200

    version 2: added error handling and validation

--- notes/models.py
+++ notes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Note(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- notes/serializers.py
+++ notes/serializers.py
@@ -3,6 +3,31 @@ from notes.models import Note
 
 
 class NotesSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(
+        required=True,
+        allow_blank=False,
+        error_messages={
+            "required": "Title is required.",
+            "blank": "Title cannot be empty.",
+            "invalid": "Title must be a string.",
+        },
+    )
+
+    content = serializers.CharField(
+        required=True,
+        allow_blank=False,
+        error_messages={
+            "required": "Content is required.",
+            "blank": "Content cannot be empty.",
+        },
+    )
+
     class Meta:
         model = Note
         fields = "__all__"
+
+    def validate_title(self, value):
+        if len(value) > 255:
+            raise serializers.ValidationError("Title can't exceed 255 characters.")
+
+        return value
